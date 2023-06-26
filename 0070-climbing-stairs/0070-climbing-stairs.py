d8320667a239efc4acbb4819ac7f831ffc43de90
PS Vishnu <psvishnu.it13@gmail.com>
2023-06-26 11:11:07 +0100

    Time: 49 ms (34.51%), Space: 16.1 MB (99.09%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        1. I:n O: num of ways 
+        2. Constraints
+        3. TC
+        4. Patterns: Divide nd conquer
+            Top down
+            
+            Current: Bottom up
+        5. Psuedo code: T:O(n) S:O(n)
+        
+        if n == 0: return 0
+        if n == 1: return 1
+        if n == 2: return 2
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        """
+        if n <= 0: return 0
+        if n == 1: return 1
+        if n == 2: return 2
+        a = 1
+        b = 2
+        for i in range(3, n+1):
+             b, a = a + b, b
+        return b
