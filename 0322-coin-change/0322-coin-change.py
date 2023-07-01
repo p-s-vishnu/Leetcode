705b96b4151ea1be619728d01b75a75330ead08a
PS Vishnu <psvishnu.it13@gmail.com>
2023-07-01 11:39:03 +0100

    Time: 1688 ms (20.33%), Space: 17.5 MB (30.60%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,10 +4,9 @@ class Solution:
         T:O(n_coins * amount) 
         S:O(amount)
         """
-        dp = [0] + [amount+1]*amount
-        
+        dp = {0:0}
+        default = amount +1 
         for c in coins:
             for i in range(c, amount+1):
-                dp[i] = min(dp[i], 1+dp[i-c])
-                
-        return -1 if dp[amount] == amount+1 else dp[amount]
+                dp[i] = min(1+dp.get(i-c, default), dp.get(i, default))
+        return -1 if dp.get(amount, default) == default else dp[amount]
