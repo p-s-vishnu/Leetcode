f36f89bbd05187eb5a97f27fc2fc19b65c55cc76
PS Vishnu <psvishnu.it13@gmail.com>
2023-07-04 19:01:21 +0100

    Time: 131 ms (27.70%), Space: 22.3 MB (20.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,10 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        T:O(m*nlog(n) )
+        S:O(1)
         d = {}
         
         for s in strs:
@@ -11,4 +14,15 @@ class Solution:
             else:
                 d[c] = [s]
         
+        return d.values()
+        
+        T:O(m*n )
+        S:O(1)
+        """
+        d = defaultdict(list)
+        for s in strs:
+            c = [0]*26
+            for char in s:
+                c[ord(char)-ord('a')] += 1
+            d[tuple(c)].append(s) 
         return d.values()
