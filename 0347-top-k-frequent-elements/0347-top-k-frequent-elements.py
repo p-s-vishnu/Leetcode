36dc7a1b11f4f4d4ddb3a2a2eca62cf583b3382b
PS Vishnu <psvishnu.it13@gmail.com>
2023-07-05 14:41:55 +0100

    Time: 121 ms (42.60%), Space: 22 MB (14.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,30 @@
+from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        T: (klogn)
         if k == len(nums):
             return nums
         
         count = Counter(nums)
         
         return heapq.nlargest(k, count.keys(), key = count.get)
+        
+        T:O(n)
+        S:O(n)
+        """
+        if k == len(nums):
+            return nums
+        freq = [[] for _ in range(len(nums)+1)]
+        c = Counter(nums)
+        for key,v in c.items():
+            freq[v].append(key)
+            
+        result = []
+        for i in range(len(nums), 0, -1):
+            for n in freq[i]:
+                result.append(n)
+                
+                if len(result) == k:
+                    return result
