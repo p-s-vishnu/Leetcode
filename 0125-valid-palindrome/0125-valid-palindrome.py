404e508a506ede92717807674a18b14a899f8491
PS Vishnu <psvishnu.it13@gmail.com>
2023-07-14 10:43:07 +0100

    Time: 92 ms (9.87%), Space: 17 MB (66.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,16 +3,20 @@ class Solution:
         """
         T:O(n)
         S:O(1)
-        
-        def isalnum(char):
-            return (ord('A') <= ord(char) <= ord('Z')) or (ord('a') <= ord(char) <= ord('z')) or (ord('0') <= ord(char) <= ord('9'))
         """
+        def isalnum(char):
+            # 0:48, A:65, a:97
+            # 9:57, Z:90, z:122
+            return (ord('A') <= ord(char) <= ord('Z')) or \
+                    (ord('a') <= ord(char) <= ord('z')) or \
+                    (ord('0') <= ord(char) <= ord('9'))
+        
         if len(s) < 2:  return True
         l, r = 0, len(s)-1
         while l < r:
-            while l < len(s) and not s[l].isalnum():
+            while l < len(s) and not isalnum(s[l]):
                 l += 1
-            while r > -1 and not s[r].isalnum():
+            while r > -1 and not isalnum(s[r]):
                 r -= 1
             if l < len(s) and r > -1 and s[l].lower() != s[r].lower():
                 return False
