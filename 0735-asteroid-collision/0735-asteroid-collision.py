9327f91a983cdf14469d1dbb3d5ebbb396865c89
PS Vishnu <psvishnu.it13@gmail.com>
2023-07-20 14:23:02 +0100

    Time: 107 ms (79.12%), Space: 17.4 MB (96.55%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,28 +3,21 @@ class Solution:
         """
         1. I: List[int] O:List[int]
         2. no need to reduce winnig asteroid, +ve right -ve left, not 0
-        3. 
+        3. TCs
             positive one side, negative other side
             mixed: one after the other 
         4. stack based, T:O(n) S:O(n)
-            two pointer?
         """
-        def collide(a, stack):
-            while stack and (stack[-1]>0 and a<0):
-                if stack[-1] + a > 0:
-                    return
-                elif stack[-1] + a == 0:
-                    stack.pop()
-                    return
-                else:
-                    stack.pop()
-            else:
-                stack.append(a)
-        
         stack = []
         for a in asteroids:
-            if stack and (stack[-1]>0 and a<0):
-                collide(a, stack)
+            while stack and (stack[-1]>0 and a<0):
+                if stack[-1] + a > 0:
+                    break
+                elif stack[-1] + a == 0:
+                    stack.pop()
+                    break
+                else:
+                    stack.pop()
             else:
                 stack.append(a)
         return stack
