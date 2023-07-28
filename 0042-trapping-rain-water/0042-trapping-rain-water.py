fb7d6acf62f92bfb1c549d383bf39c95fe2bd1c9
PS Vishnu <psvishnu.it13@gmail.com>
2023-07-28 10:35:38 +0100

    Time: 119 ms (96.96%), Space: 18.4 MB (41.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -9,20 +9,36 @@ class Solution:
         4. Idea: know water retention at each point
             Prefix and suffix array T:O(n) S:O(n)
             Two pointer T:O(n) S:O(1)
+            
+        5. Prefix/Stack
+            def get_cumulative_max(arr):
+                out = []
+                max_so_far = 0
+                for h in arr:
+                    out.append(max_so_far)
+                    max_so_far = max(max_so_far, h)
+                return out
+
+            totAr = 0
+            maxL = get_cumulative_max(height)
+            maxR = get_cumulative_max(height[::-1])[::-1]
+            for i, h in enumerate(height):
+                totAr += max(min(maxL[i], maxR[i])-h, 0)
+            return totAr
         """
         if len(height) <= 2: return 0
-        def get_cumulative_max(arr):
-            out = []
-            max_so_far = 0
-            for h in arr:
-                out.append(max_so_far)
-                max_so_far = max(max_so_far, h)
-            return out
-        
+        l, r = 0, len(height)-1
         totAr = 0
-        maxL = get_cumulative_max(height)
-        maxR = get_cumulative_max(height[::-1])[::-1]
-        for i, h in enumerate(height):
-            totAr += max(min(maxL[i], maxR[i])-h, 0)
+        maxL, maxR = height[l], height[r]
+        
+        while l < r:
+            if height[l] < height[r]:
+                l += 1
+                maxL = max(maxL, height[l])
+                totAr += maxL - height[l]
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                totAr += maxR - height[r]
         return totAr
         
