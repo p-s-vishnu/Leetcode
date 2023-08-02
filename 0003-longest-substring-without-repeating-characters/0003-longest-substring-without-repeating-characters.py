0b6e448c35a1e74ebbabf462d88e3ed073c76fd8
PS Vishnu <psvishnu.it13@gmail.com>
2023-08-02 11:35:47 +0100

    Time: 52 ms (98.93%), Space: 16.3 MB (80.47%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,10 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        Sliding window problem
+        T: O(n) S:(n or 26->1)
+        
         """
-        if len(s) < 2:
-            return len(s)
+        if len(s) < 2: return len(s)
+        l = longestSubstr = -1
+        num_idx_map = {}
+        for r in range(len(s)):
+            if s[r] in num_idx_map and num_idx_map[s[r]] > l:   l = num_idx_map[s[r]]
+            num_idx_map[s[r]] = r
+            longestSubstr = max(longestSubstr, r-l)
+        return longestSubstr
+        
+        
+        """
+        Alternate Sliding window problem using sets
+        if len(s) < 2: return len(s)
         l = 0
         longest = 0
         visited = set()
@@ -15,17 +27,4 @@ class Solution:
             visited.add(s[r])
             longest = max(longest, r-l+1)
         return longest
-        
-"""
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        l = -1
-        longest = 0
-        alphabets = {}
-        for r in range(len(s)):
-            if (s[r] in alphabets) and alphabets[s[r]] > l:
-                l = alphabets[s[r]]
-            alphabets[s[r]] = r
-            longest = max(longest, r-l)
-        return longest
-"""
+        """
